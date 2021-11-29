7b0daef4ae92459bb23b4b3fd287111cc4a2efca
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-11-29 18:00:16 +0100

    start project

---
+++ src/main/kotlin/com/forbusypeople/budgetK/BudgetKApplication.kt
@@ -0,0 +1,11 @@
+package com.forbusypeople.budgetK
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetKApplication
+
+fun main(args: Array<String>) {
+	runApplication<BudgetKApplication>(*args)
+}
---
+++ src/test/kotlin/com/forbusypeople/budgetK/BudgetKApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.forbusypeople.budgetK
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetKApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
