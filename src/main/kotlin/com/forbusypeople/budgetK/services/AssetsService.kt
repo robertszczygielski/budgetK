2be7d755a99856a2be650b1873d5da21bc789d75
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-12-30 15:59:41 +0100

    (003) Zapis i doczyt z bazy NoSQL - MongoDB; Kotlin; Docker

--- src/main/kotlin/com/forbusypeople/budgetK/services/AssetsService.kt
+++ src/main/kotlin/com/forbusypeople/budgetK/services/AssetsService.kt
@@ -1,6 +1,8 @@
 package com.forbusypeople.budgetK.services
 
 import com.forbusypeople.budgetK.controllers.AssetDto
+import com.forbusypeople.budgetK.repository.AssetEntity
+import com.forbusypeople.budgetK.repository.AssetsRepository
 import org.springframework.stereotype.Service
 
 interface AssetsService {
@@ -10,12 +12,28 @@ interface AssetsService {
 
 @Service
 class AssetServiceImpl(
-    private var dots: List<AssetDto>
+    private val assetsRepository: AssetsRepository
 ): AssetsService {
-    override fun getAssets(): List<AssetDto> = dots
+    override fun getAssets(): List<AssetDto> = assetsRepository.findAll()
+        .map{ it.toDto() }
 
     override fun saveAssets(dtoList: List<AssetDto>) {
-        this.dots = dtoList
+        assetsRepository.saveAll(dtoList.map { it.toEntity() })
     }
 
 }
+
+private fun AssetDto.toEntity() = AssetEntity(
+    amount = this.amount,
+    category = this.category,
+    incomeDate = this.incomeDate,
+    description = this.description
+)
+
+private fun AssetEntity.toDto() = AssetDto(
+    id = this.id,
+    amount = this.amount,
+    category = this.category,
+    incomeDate = this.incomeDate,
+    description = this.description
+)
